9d1b428b5ae86cf34a5c74536ae755b958ed0553
Mayank kadam <mayankkadam1039@gmail.com>
2024-06-26 01:08:25 +0530

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,19 @@
 #include <iostream>
 
-int main() {
+int main()
+{
   // Flush after every std::cout / std:cerr
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
 
-
-   while(true) {
+  while (true)
+  {
     std::cout << "$ ";
     std::string input;
     std::getline(std::cin, input);
+
+    if (input == "exit 0")
+      return 0;
     std::cout << input << ": command not found" << std::endl;
   }
 }
