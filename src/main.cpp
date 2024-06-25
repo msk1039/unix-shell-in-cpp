9c6affaa10e2681b3bb1b9e5d5332c353af0c7ab
Mayank kadam <mayankkadam1039@gmail.com>
2024-06-26 01:25:06 +0530

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 
 int main()
 {
+  
   // Flush after every std::cout / std:cerr
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
@@ -12,8 +13,21 @@ int main()
     std::string input;
     std::getline(std::cin, input);
 
+    size_t start = 0;
+    size_t end = input.length() - 1;
+
+    std::string firstWord = input.substr(0, input.find(" "));
+
     if (input == "exit 0")
       return 0;
+    else if(firstWord == "echo"){
+      std::string echo = input.substr(5);
+      std::cout << echo << std::endl;
+    }
+    else{
     std::cout << input << ": command not found" << std::endl;
+
+    }
+  
   }
 }
